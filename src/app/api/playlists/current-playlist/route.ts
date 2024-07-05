c2fd24402b4348b862db958662f16d1347388a13
Romain Bidault <romain.bidault@supdevinci-edu.fr>
2024-07-05 11:05:38 +0200

    Adding return for nextResponse

--- src/app/api/playlists/current-playlist/route.ts
+++ src/app/api/playlists/current-playlist/route.ts
@@ -4,16 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export const GET = async (req: NextRequest) => {
-  try {
   const now = new Date();
   const currentHour = now.getHours();
   const currentMinutes = now.getMinutes();
-  const currentWeekDay = now.toLocaleString('en-US', { weekday: 'long' });
+  const currentWeekDay = now.toLocaleString("en-US", { weekday: "long" });
 
   // Convert the current time to a comparable format
   const currentTime = new Date(now.setHours(currentHour, currentMinutes, 0, 0));
 
-  
+  try {
     const currentSlot = await prisma.slot.findFirst({
       where: {
         WeekDay: currentWeekDay,
@@ -30,11 +29,14 @@ export const GET = async (req: NextRequest) => {
     });
 
     if (!currentSlot || currentSlot.Playlists.length === 0) {
-      NextResponse.json({ error: 'No playlist found for the current time and day' },{status:404});
+      return NextResponse.json(
+        { error: "No playlist found for the current time and day" },
+        { status: 404 }
+      );
     } else {
-      NextResponse.json(currentSlot.Playlists[0]); 
+      return NextResponse.json(currentSlot.Playlists[0]);
     }
   } catch (error) {
-    NextResponse.json({ error: error });
+    return NextResponse.json({ error: error });
   }
-}
+};
